3e69251d2e21e37dbaba22fe3e06c1cd8102efb8
shivprajapat <shivkumarprajapat232@gmail.com>
2023-08-12 17:24:53 +0530

    feat: 🎸 billboard banner ui

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { getBillboard } from "@/actions/get-billboard";
+import Billboard from "@/components/ui/billboard";
+import React from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const billboard = await getBillboard("aa00ac9c-b72e-41b7-9e33-8ad84e341931");
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <Billboard data={billboard} />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
