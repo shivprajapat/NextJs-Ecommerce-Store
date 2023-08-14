13f8a0362fc3bba3edadd6f35fd0bdd631c7d7c9
shivprajapat <shivkumarprajapat232@gmail.com>
2023-08-14 12:48:44 +0530

    feat: 🎸 featured product

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,25 @@
-import { getBillboard } from "@/actions/get-billboard";
-import Billboard from "@/components/ui/billboard";
 import React from "react";
 
+import { getBillboard } from "@/actions/get-billboard";
+import Billboard from "@/components/ui/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+import { getProducts } from "@/actions/get-products";
+
 const HomePage = async () => {
-  const billboard = await getBillboard("aa00ac9c-b72e-41b7-9e33-8ad84e341931");
+  const billboard = await getBillboard("58fbf43d-c657-487c-adc1-3c3f1e72140a");
+
+  const products = await getProducts({ isFeatured: true });
+
   return (
-    <div>
-      <Billboard data={billboard} />
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 maxh">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
   );
 };
 
