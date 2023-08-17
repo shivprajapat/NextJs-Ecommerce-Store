12ccc0d957d1b3735820961150213430314b0225
shivprajapat <shivkumarprajapat232@gmail.com>
2023-08-17 09:29:31 +0530

    fix: 🐛 api route change

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import useCart from "@/hooks/use-cart";
 import Container from "@/components/ui/container";
@@ -8,12 +8,23 @@ import CartItem from "./components/cart-item";
 import Summary from "./components/summary";
 
 const CartPage = () => {
+  const [isMounted, setIsMounted] = useState(false);
   const cart = useCart();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="bg-white">
       <Container>
         <div className="px-4 py-7 sm:py-16 sm:px-6 lg:px-8">
-          <h1 className="text-xl md:text-3xl font-bold text-black">Shopping Cart</h1>
+          <h1 className="text-xl md:text-3xl font-bold text-black">
+            Shopping Cart
+          </h1>
           <div className="md:mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7 h-full">
               {/* TODO: Show me the Error */}
